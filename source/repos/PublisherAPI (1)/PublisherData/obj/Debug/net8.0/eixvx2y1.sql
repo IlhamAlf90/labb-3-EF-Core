82f446ac28a7142fbcafbc86f1ef81fc733f0f5e
Ilham Alfayyad <147632236+IlhamAlf90@users.noreply.github.com>
2025-03-13 11:09:22 +0100

    test

---
+++ source/repos/PublisherAPI (1)/PublisherData/obj/Debug/net8.0/eixvx2y1.sql
@@ -0,0 +1,26 @@
+﻿BEGIN TRANSACTION;
+GO
+
+DROP TABLE [ArtistCover];
+GO
+
+CREATE TABLE [CoverAssignment] (
+    [ArtistId] int NOT NULL,
+    [CoverId] int NOT NULL,
+    [DateCreated] datetime2 NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    CONSTRAINT [PK_CoverAssignment] PRIMARY KEY ([ArtistId], [CoverId]),
+    CONSTRAINT [FK_CoverAssignment_Artists_ArtistId] FOREIGN KEY ([ArtistId]) REFERENCES [Artists] ([ArtistId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_CoverAssignment_Covers_CoverId] FOREIGN KEY ([CoverId]) REFERENCES [Covers] ([CoverId]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_CoverAssignment_CoverId] ON [CoverAssignment] ([CoverId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231202161452_testpayload', N'8.0.0');
+GO
+
+COMMIT;
+GO
+
---
+++ source/repos/PublisherAPI (1)/PublisherData/obj/Debug/net8.0/vjom4fet.sql
@@ -0,0 +1,37 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [Covers] ADD [BookId] int NOT NULL DEFAULT 0;
+GO
+
+UPDATE [Covers] SET [BookId] = 0
+WHERE [CoverId] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [Covers] SET [BookId] = 0
+WHERE [CoverId] = 2;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [Covers] SET [BookId] = 0
+WHERE [CoverId] = 3;
+SELECT @@ROWCOUNT;
+
+GO
+
+CREATE UNIQUE INDEX [IX_Covers_BookId] ON [Covers] ([BookId]);
+GO
+
+ALTER TABLE [Covers] ADD CONSTRAINT [FK_Covers_Books_BookId] FOREIGN KEY ([BookId]) REFERENCES [Books] ([BookId]) ON DELETE CASCADE;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231202194001_bookcoverrelationship', N'8.0.0');
+GO
+
+COMMIT;
+GO
+
